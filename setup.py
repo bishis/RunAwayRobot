3b98bf347f454130440c532a707ded2236677d13
Ahmed Mazher <ahmed.mazher@student.manchester.ac.uk>
2024-12-07 21:43:24 +0000

    removed cmake

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.py'))),  # Ensure this line exists
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.py'))),
+        ('share/' + package_name + '/config', glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

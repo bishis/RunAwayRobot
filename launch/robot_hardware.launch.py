31dcec47deba2e370c2d0664e9045f70af4a9fa0
Ahmed Mazher <a.mazher1014@gmail.com>
2024-12-29 22:11:26 +0000

    rename

--- launch/robot_hardware.launch.py
+++ launch/robot_hardware.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 os.path.join(get_package_share_directory('rplidar_ros'),
-                           'launch', 'rplidar.launch.py')
+                           'launch', 'rplidar_a1_launch.py')
             ])
         ),
 

a46ebd75135af48d32a7c2273eff8972b6a0e972
Ahmed Mazher <a.mazher1014@gmail.com>
2025-01-20 01:30:11 +0000

    ddd

--- launch/robot_hardware.launch.py
+++ launch/robot_hardware.launch.py
@@ -37,6 +37,6 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_link_to_laser',
-            arguments=['0', '0', '0.18', '0', '0', '0', 'base_link', 'laser']
+            arguments=['0', '0', '0.18', '0', '0', '0', 'base_link', 'laser_frame']
         )
     ])

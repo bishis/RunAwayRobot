3ef71ea327d1b59eaf6087bc8d94110b71b92a9e
Ahmed Mazher <a.mazher1014@gmail.com>
2025-01-20 02:02:50 +0000

    revert

--- launch/slam_navigation.launch.py
+++ launch/slam_navigation.launch.py
@@ -41,11 +41,10 @@ def generate_launch_description():
             package='rf2o_laser_odometry',
             executable='rf2o_laser_odometry_node',
             name='rf2o_laser_odometry',
-            output='screen',
             parameters=[{
                 'laser_scan_topic': '/scan',
                 'odom_topic': '/odom_rf2o',
-                'publish_tf': 'true',
+                'publish_tf': True,
                 'base_frame_id': 'base_link',
                 'odom_frame_id': 'odom',
                 'init_pose_from_topic': '',

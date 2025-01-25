c93f85e2294c646c7480b0cdbe191dea2ee85be3
Ahmed Mazher <a.mazher1014@gmail.com>
2025-01-25 23:36:32 +0000

    dd

--- launch/gazebo_simulation.launch.py
+++ launch/gazebo_simulation.launch.py
@@ -4,11 +4,15 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.substitutions import Command
 
 def generate_launch_description():
     pkg_dir = get_package_share_directory('motor_controller')
     gazebo_ros_dir = get_package_share_directory('gazebo_ros')
-
+    
+    # Process XACRO file
+    xacro_file = os.path.join(pkg_dir, 'urdf', 'robot.urdf.xacro')
+    
     # Robot state publisher
     robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -16,7 +20,8 @@ def generate_launch_description():
         name='robot_state_publisher',
         output='screen',
         parameters=[{
-            'robot_description': open(os.path.join(pkg_dir, 'urdf', 'robot.urdf.xacro')).read()
+            'robot_description': Command(['xacro ', xacro_file]),
+            'use_sim_time': True
         }]
     )
 
@@ -24,14 +29,15 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, 'launch', 'gazebo.launch.py'),
-        )
+        ),
+        launch_arguments={'verbose': 'false'}.items()
     )
 
     # Spawn robot
     spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description',
+        arguments=['-topic', '/robot_description',
                   '-entity', 'my_robot',
                   '-x', '0',
                   '-y', '0',

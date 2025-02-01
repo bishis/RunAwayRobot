fd4e2ec9d1441ee8edcab9fe79e4df11d24d61d8
Ahmed Mazher <a.mazher1014@gmail.com>
2025-02-01 01:23:48 +0000

    s

--- launch/robot_hardware.launch.py
+++ launch/robot_hardware.launch.py
@@ -78,7 +78,7 @@ def generate_launch_description():
             name='image_compress',
             arguments=['raw', 'compressed'],
             remappings=[
-                ('in', 'image_raw'),
+                ('in', '/camera/image_raw'),
                 ('out/compressed', '/camera/image_raw/compressed'),
             ]
         )

23fe97caa96b09ff4d30997b99de5e1434f2b795
Ahmed Mazher <a.mazher1014@gmail.com>
2025-03-04 17:12:31 +0000

    f

--- motor_controller/controllers/motor_controller.py
+++ motor_controller/controllers/motor_controller.py
@@ -61,7 +61,7 @@ class MotorController:
         """
             
         # Scale the entire range from MIN_SPEED to 1.0
-        MIN_SPEED = 0.85
+        MIN_SPEED = 0.825
 
         def scale_to_min_speed(speed: float) -> float:
             if speed == 0.0:
